9d6eaf2cd8eac1d2824256959c2e37dde9d9c22f
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2021-08-06 17:18:44 -0300

    fc

---
+++ prodoct-catalog2/src/main/java/one/digitalinnovation/experts/prodoctcatalog2/ProdoctCatalog2Application.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.prodoctcatalog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdoctCatalog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdoctCatalog2Application.class, args);
+	}
+
+}
---
+++ prodoct-catalog2/src/main/java/one/digitalinnovation/experts/prodoctcatalog2/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.experts.prodoctcatalog2.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<ProductRepository, Integer> {
+}
